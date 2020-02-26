59fd14497d3822f9852f0fe325024a485dba1e66
MoranLeven <49155163+MoranLeven@users.noreply.github.com>
2020-02-27 00:44:09 +0530

    edited

--- DailyCodingProblems/Problems & Solutions/Problem2/Problem2.py
+++ DailyCodingProblems/Problems & Solutions/Problem2/Problem2.py
@@ -3,49 +3,36 @@ arr = [1,2,3,4,5]
 
 
 def except_index_multiplier(arr):
-
     n = len(arr)
-
     left = [0]*n
-
     right = [0]*n
-
     product = [0]*n
-
     left[0]= 1
-
     right[n-1] = 1
 
     for i in range(1,n):
-
         left[i] = arr[i-1] * left[i-1]
 
     for j in range(n-2,-1,-1):
-
         right[j] = arr[j+1]*right[j+1]
 
     for i in range(n):
-
         product[i] = left[i] * right[i]
 
     return product
 
 print(except_index_multiplier(arr))
 
-
-
 #This solution is faster and better O(1) kinda off using only one loop :) --Badass Way-- 
-
 def except_index_multiplier2(arr):
-
     if len(arr) == 1:
         print(0)
         return
-
-
+    
     i, temp = 1, 1
     n = len(arr) 
     product = [0]*n #allocating memory for array
+    #product = [0 for i in range(n)] == another way to allocate memory to array 
 
     for i in range(n):
         product[i] = temp
